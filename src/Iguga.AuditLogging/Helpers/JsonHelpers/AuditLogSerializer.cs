7ddde51f9009742476859a3369ac172f5c53ceca
Ivan Mota <ivan.sv350@iguga.net>
2022-11-05 10:39:43 +0000

    Remove the dependencie of Newtonsoft.json

--- src/Iguga.AuditLogging/Helpers/JsonHelpers/AuditLogSerializer.cs
+++ src/Iguga.AuditLogging/Helpers/JsonHelpers/AuditLogSerializer.cs
@@ -1,12 +1,6 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-// Original file: https://github.com/IdentityServer/IdentityServer4/blob/master/src/IdentityServer4/src/Logging/LogSerializer.cs
-// Modified by Jan Škoruba
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Formatting = Newtonsoft.Json.Formatting;
+﻿using Iguga.AuditLogging.Parsing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Iguga.AuditLogging.Helpers.JsonHelpers
 {
@@ -15,25 +9,23 @@ namespace Iguga.AuditLogging.Helpers.JsonHelpers
     /// </summary>
     public static class AuditLogSerializer
     {
-        public static readonly JsonSerializerSettings DefaultAuditJsonSettings = new JsonSerializerSettings
+        public static readonly JsonSerializerOptions DefaultAuditJsonSettings = new()
         {
-            NullValueHandling = NullValueHandling.Ignore,
-            DateFormatHandling = DateFormatHandling.IsoDateFormat,
-            Formatting = Formatting.Indented
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            WriteIndented = true
         };
 
-        public static readonly JsonSerializerSettings BaseAuditEventJsonSettings = new JsonSerializerSettings
+        public static readonly JsonSerializerOptions BaseAuditEventJsonSettings = new()
         {
-            NullValueHandling = NullValueHandling.Ignore,
-            DateFormatHandling = DateFormatHandling.IsoDateFormat,
-            Formatting = Formatting.Indented
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            WriteIndented = true
         };
 
         static AuditLogSerializer()
         {
-            DefaultAuditJsonSettings.Converters.Add(new StringEnumConverter());
-            BaseAuditEventJsonSettings.Converters.Add(new StringEnumConverter());
-            BaseAuditEventJsonSettings.ContractResolver = new AuditLoggerContractResolver();
+            DefaultAuditJsonSettings.Converters.Add(new JsonStringEnumConverter());
+            BaseAuditEventJsonSettings.Converters.Add(new JsonStringEnumConverter());
+            BaseAuditEventJsonSettings.Converters.Add(new IgnoreAuditEventPropertiesJsonConverter());
         }
 
         /// <summary>
@@ -43,7 +35,7 @@ namespace Iguga.AuditLogging.Helpers.JsonHelpers
         /// <returns></returns>
         public static string Serialize(object logObject)
         {
-            return JsonConvert.SerializeObject(logObject, DefaultAuditJsonSettings);
+            return JsonSerializer.Serialize(logObject, DefaultAuditJsonSettings);
         }
 
         /// <summary>
@@ -52,9 +44,9 @@ namespace Iguga.AuditLogging.Helpers.JsonHelpers
         /// <param name="logObject">The object.</param>
         /// <param name="settings"></param>
         /// <returns></returns>
-        public static string Serialize(object logObject, JsonSerializerSettings settings)
+        public static string Serialize(object logObject, JsonSerializerOptions settings)
         {
-            return JsonConvert.SerializeObject(logObject, settings);
+            return JsonSerializer.Serialize(logObject, settings);
         }
     }
 }
---
+++ src/Iguga.AuditLogging/Parsing/IgnoreAuditEventPropertiesJsonConverter.cs
@@ -0,0 +1,80 @@
+﻿using Iguga.AuditLogging.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Iguga.AuditLogging.Parsing
+{
+    public class IgnoreAuditEventPropertiesJsonConverter : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            if (typeToConvert != typeof(AuditEvent))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            return new IgnoreAuditEventPropertiesJsonConverterInner(options);
+        }
+
+        private class IgnoreAuditEventPropertiesJsonConverterInner : JsonConverter<AuditEvent>
+        {
+            private readonly JsonSerializerOptions _options;
+
+            /// <summary>
+            /// Ignore base properties of audit event, because these properties are stored separately
+            /// </summary>
+            private readonly List<string> _propertiesToIgnore = new()
+            {
+                nameof(AuditEvent.Category),
+                nameof(AuditEvent.SubjectAdditionalData),
+                nameof(AuditEvent.Action),
+                nameof(AuditEvent.SubjectIdentifier),
+                nameof(AuditEvent.SubjectType),
+                nameof(AuditEvent.SubjectName),
+                nameof(AuditEvent.Event),
+                nameof(AuditEvent.Source)
+            };
+
+            public IgnoreAuditEventPropertiesJsonConverterInner(JsonSerializerOptions options)
+            {
+                _options = options;
+            }
+
+            public override AuditEvent Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                return JsonSerializer.Deserialize<AuditEvent>(ref reader, _options);
+            }
+
+            public override void Write(Utf8JsonWriter writer, AuditEvent value, JsonSerializerOptions options)
+            {
+                if (value is not null)
+                {
+                    writer.WriteStartObject();
+
+                    foreach (var property in value.GetType().GetProperties().Where(c => !_propertiesToIgnore.Contains(c.Name)))
+                    {
+                        var propValue = property.GetValue(value);
+                        switch (true)
+                        {
+                            case true when propValue is not null:
+                            case true when propValue is null && options.DefaultIgnoreCondition == JsonIgnoreCondition.Never:
+                                writer.WritePropertyName(property.Name);
+                                JsonSerializer.Serialize(writer, propValue, options);
+                                break;
+                        }
+                    }
+
+                    writer.WriteEndObject();
+                }
+            }
+        }
+    }
+}
